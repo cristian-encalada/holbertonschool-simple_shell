703f6dfe3ec18133a9a03a12755be57e496ad647
Hiojam <hiojam2016@gmail.com>
2023-04-17 15:10:23 -0500

    First version which includes reading and executing CMDs

---
+++ main.c
@@ -0,0 +1,95 @@
+#include "shell.h"
+
+/**
+ * split_str - Splits a string.
+ * 
+ * @s: String to be splitted.
+ * Return: An array of words.
+*/
+char **split_str(char *s)
+{
+  char **splitted = malloc(sizeof(char) * strlen(s));
+  unsigned int i = 0, a = 0;
+
+  if (!splitted)
+    return (NULL);
+
+  for (; s[i]; i++)
+  {
+    if (s[i] == ' ')
+    {
+      a++;
+      continue;
+    }
+    splitted[a][i] = s[i];
+  }
+
+  return (splitted);
+}
+
+/**
+ * call_command - Call an executable
+ * 
+ * @command: String containing the command name.
+ * Return: void
+*/
+void call_command(char *command)
+{
+  char *argv[] = {"", NULL};
+
+  if (execve(command, argv, NULL) == -1)
+  {
+    perror("Error");
+  }
+}
+
+/**
+ * read_line - Logic for reading user input.
+ * 
+ * Return: 1 on Success.
+*/
+int read_line(){
+  char *buffer;
+  size_t bufsize = 1024;
+  int chars_read = 0;
+
+  buffer = (char *) malloc(bufsize * sizeof(char));
+  if(!buffer)
+  {
+    perror("Unable to allocate buffer");
+    exit(1);
+  }
+
+  while (1)
+  {
+    printf("$ ");
+    chars_read = getline(&buffer, &bufsize, stdin);
+    
+    if (buffer[chars_read - 1] != '\n'){
+      printf("\n");
+      exit(1);
+    }
+
+    buffer[chars_read - 1] = '\0';
+    call_command(buffer);
+  }
+
+  free(buffer);
+  return (1);
+}
+
+/**
+ * main - Main entry of the program.
+ * 
+ * @argc: amount of arguments passed.
+ * @argv: an array of arguments.
+ * Return: 0 on Success, -1 on Error.
+*/
+int main(int argc, char *argv[])
+{
+  (void) argc;
+  (void) argv;
+
+  read_line();
+  return (0);
+}
---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <unistd.h>
+#include <dirent.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#endif
