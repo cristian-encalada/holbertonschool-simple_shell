49d740cc3e64f0754f957089c373bb5c94bf2e55
Hiojam <hiojam2016@gmail.com>
2023-04-18 12:25:17 -0500

    Added arguments to command line

--- main.c
+++ main.c
@@ -1,6 +1,61 @@
 #include "shell.h"
 
+int str_count_words(char *s, const char *delim){
+  char *tok;
+  int numWords;
+  tok = strtok(strdup(s), delim);         /* Get first token */
 
+  while (tok != NULL)               /* Check for delimiters */
+  {
+    numWords++;
+    tok = strtok(NULL, delim);    /* Go through other tokens */
+  }
+  return (numWords);
+}
+
+/**
+ * split_str - Splits a string.
+ * @s: String to be splitted.
+ *
+ * Return: Void
+*/
+char **split_str(char *str)
+{
+  const char *delim = " ";         /* Delimiter */
+  char *tok;
+  char **array;
+  unsigned int len = 0, numWords = str_count_words(str, delim), count = 0, i = 0;
+
+  array = (char **) malloc((numWords + 1) * sizeof(char *));
+
+  if (array == NULL)
+    return (NULL);
+  
+  tok = strtok(str, delim);
+
+  while (tok != NULL)
+  {
+    for (len = 0; tok[len]; len++)
+      ;
+
+    array[count] = (char *) malloc((len + 1) * sizeof(char));
+    if (array[count] == NULL)
+      return (NULL);
+
+    for (i = 0; i < len; i++)
+    {
+      array[count][i] = tok[i];
+    }
+    
+    array[count][i] = '\0';
+
+    count++;
+    tok = strtok(NULL, delim);
+  }
+
+  array[count] = NULL;
+  return (array);
+}
 
 /**
  * call_command - Call an executable
@@ -19,7 +74,7 @@ void call_command(char *command)
     perror("Error");
   else if (pid == 0)
   {
-    if (execve(command, argv, NULL) == -1)
+    if (execve(argv[0], argv, NULL) == -1)
       perror("Error");
   }
   else
@@ -55,7 +110,6 @@ int read_line(){
 
     buffer[chars_read - 1] = '\0';
     call_command(buffer);
-    split_str(buffer);
   }
 
   free(buffer);
